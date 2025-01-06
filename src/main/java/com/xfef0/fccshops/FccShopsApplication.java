6eac7b74422f9f26c14312adba65027babeb8fc0
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-01-06 09:53:17 -0300

    Project created through Spring Initializr

---
+++ src/main/java/com/xfef0/fccshops/FccShopsApplication.java
@@ -0,0 +1,13 @@
+package com.xfef0.fccshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FccShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FccShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xfef0/fccshops/FccShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfef0.fccshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FccShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
