6363f329a78f2e4a739590ac9142e94faa297ef8
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-01-06 10:58:25 -0300

    add classes that model our shop

---
+++ src/main/java/com/xfef0/fccshops/model/Image.java
@@ -0,0 +1,31 @@
+package com.xfef0.fccshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    private String downloadURL;
+
+    @Lob
+    private Blob image;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
