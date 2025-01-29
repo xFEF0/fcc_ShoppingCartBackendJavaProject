59b81bbea2058afface1eadfc183b53ff179a7e5
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-01-29 12:20:49 -0300

    update configuration

---
+++ src/main/java/com/xfef0/fccshops/config/ShopConfig.java
@@ -0,0 +1,14 @@
+package com.xfef0.fccshops.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShopConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
