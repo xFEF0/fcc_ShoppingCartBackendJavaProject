493cf91f50407fe024ab3517edf82729d975b6ae
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-01-29 12:36:34 -0300

    add Cart entities

---
+++ src/main/java/com/xfef0/fccshops/model/Cart.java
@@ -0,0 +1,26 @@
+package com.xfef0.fccshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> cartItems;
+}
