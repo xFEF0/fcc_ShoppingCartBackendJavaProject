d4615f9b3703d7433d18b017a8201e700f318746
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-06 13:35:57 -0300

    add JWTUtils

---
+++ src/main/java/com/xfef0/fccshops/security/jwt/JWTUtils.java
@@ -0,0 +1,60 @@
+package com.xfef0.fccshops.security.jwt;
+
+import com.xfef0.fccshops.security.user.ShopUserDetails;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JWTUtils {
+
+    @Value("${auth.token.expirationInMils}")
+    private int expirationTime;
+
+    public String generateTokenForUser(Authentication authentication) {
+        ShopUserDetails shopUserDetails = (ShopUserDetails) authentication.getPrincipal();
+        List<String> roles = shopUserDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        return Jwts.builder()
+                .subject(shopUserDetails.getEmail())
+                .claim("id", shopUserDetails.getId())
+                .claim("roles", roles)
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + expirationTime))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String getUserNameFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new JwtException(e.getMessage());
+        }
+    }
+
+    private SecretKey getSigningKey() {
+        return Jwts.SIG.HS256.key().build();
+    }
+}
